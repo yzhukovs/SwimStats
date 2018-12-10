fa6bd0e801305edff890317d1038e94561c039ef
yzhukovs <yzhukovs@bumnetworks.com>
2018-12-10 00:10:02 -0500

    hardcoded ids

--- SwimStats/AppDelegate.swift
+++ SwimStats/AppDelegate.swift
@@ -11,31 +11,38 @@ import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
-
-
+    let lsc = "MR"
+    let agua = "Asphalt Green Unified Aquatics"
+    var aguaID: String!
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
         let db = Firestore.firestore()
         
-        db.collection("LSCs")
+        db
+            .collection("LSCs")
+            .document(lsc)
+            .collection("Clubs")
+            .whereField("name", isEqualTo: agua)
             .getDocuments() { (querySnapshot, err) in
                 if let err = err {
                     print("Error getting documents: \(err)")
                 } else {
                     for document in querySnapshot!.documents {
-                        print("\(document.documentID) => \(document.data())")
+                        self.aguaID = document.documentID
+                        break
                     }
                 }
         }
         // Override point for customization after application launch.
         return true
     }
-
-
     
     
     
-    }
+    
+    
+}
 
